51022a23c93f888a7a3bce2b201759fa8870d7ac
GERI12326 <114013384+GERI12326@users.noreply.github.com>
2023-04-26 12:59:52 +0200

    components

---
+++ src/components/Import.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router";
+import axios from "axios";
+import { useState } from "react";
+
+const Import = () => {
+  return (
+    <div>
+      <div>
+        <div>Buying Request</div>
+        <div>Add request free</div>
+      </div>
+      <div>
+        {importItems.map((importItem) => {
+          return (
+            <div>
+              {categories.map((category) => {
+                return <div>{category.name}</div>;
+              })}
+            </div>
+          );
+          <div></div>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Import;
