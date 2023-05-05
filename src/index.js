d17d4deaeb170a050df50d1148d1913a45dbee13
rrezonk16 <rrezonkrasniqi32@gmail.com>
2023-05-05 10:14:50 +0200

    Removed axios from the index file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import axios from "axios";
 import App from "./App";
-import axios from "axios";
+
 import { BrowserRouter as Router } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
