6ba04be1abdd2d6d5ddd32df2b9d4e9e174e2e50
rrezonk16 <rrezonkrasniqi32@gmail.com>
2023-05-11 11:32:42 +0200

    Function doesnt allow user to buy

--- src/components/Products/ProductSellTest.js
+++ src/components/Products/ProductSellTest.js
@@ -1,34 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import NavBar from "../Navigation/NavBar";
+
 export const ProductSellTest = () => {
-  const price = 100;
+  const price = 120;
+  const [loading, setLoading] = useState(false);
+  const [tokens, setTokens] = useState(localStorage.getItem("tokens"));
 
   const handleBuy = async () => {
     console.log("Buy button clicked");
-    const tokens = localStorage.getItem("tokens");
-    await axios.put("http://localhost:8000/api/updateToken/11", {
-      amount: tokens - price,
-    });
+    
+    if (tokens < price) {
+      alert("Not enough tokens!");
+      return;
+    }
+    setLoading(true);
+    try {
+      await axios.put("http://localhost:8000/api/updateToken/11", {
+        amount: tokens - price,
+      });
+      setLoading(false);
+      // Handle success or redirect to a success page
+    } catch (error) {
+      setLoading(false);
+      // Handle error
+    }
+    const response = await axios.get("http://localhost:8000/api/token/11");
+    setTokens(response.data.amount);
   };
+
+
 
   return (
     <div>
       <NavBar />
-      <h1>This is a test site</h1>
-      <br />
-      <h2>Product Name: Test</h2>
-      <br />
-      <br />
-      <br />
+      {loading ? (
+        <div className="loading-screen">
+          <p>Loading...</p>
+        </div>
+      ) : (
 
-      <h3>Product Description: Test product</h3>
-      <br />
+        <div>
+            <p>{tokens}</p>
+          <h1>This is a test site</h1>
+          <br />
+          <h2>Product Name: Test</h2>
+          <br />
+          <br />
+          <br />
 
-      <p>Product Price: {price}</p>
-      <br />
+          <h3>Product Description: Test product</h3>
+          <br />
 
-      <button onClick={handleBuy}>Buy</button>
+          <p>Product Price: {price}</p>
+          <br />
+
+          <button onClick={handleBuy}>Buy</button>
+        </div>
+      )}
     </div>
   );
 };
