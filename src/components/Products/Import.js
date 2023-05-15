444b46c4264593625647d5b6e5b9a9c04f6054b4
Bumirec <bumirec@gmail.com>
2023-05-15 09:00:54 +0200

    bug fix on import/export filters

--- src/components/Products/Import.js
+++ src/components/Products/Import.js
@@ -3,29 +3,65 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import moment from "moment";
 import Button from "react-bootstrap/Button";
-import NavBar from "../Navigation/NavBar";
+import Form from "react-bootstrap/Form";
+
 const Import = () => {
   const navigate = useNavigate();
   const [importProducts, setImportProducts] = useState([]);
-  //calling the GET method to get the "import" data
-   const getImportProducts = async () => {
-     const data = {
-       headers: {
-         Accept: "application/json",
-       },
-     };
-     const apiImportProducts = await axios.get(
-       "http://127.0.0.1:8000/api/ilist",
-       data
-     );
-     setImportProducts(apiImportProducts.data.data);
-   };
-   useEffect(() => {
-     getImportProducts();
-   }, []);
-   const handleNavigateItem = (id) => {
-     navigate("/ImportItem/" + id);
-   };
+  const [categories, setCategories] = useState([]);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
+  const getImportProducts = async () => {
+    const data = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const apiImportProducts = await axios.get(
+      "http://127.0.0.1:8000/api/ilist",
+      data
+    );
+    setImportProducts(apiImportProducts.data.data);
+  };
+
+  const getCategories = async () => {
+    const data = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const apiCategories = await axios.get(
+      "http://127.0.0.1:8000/api/productcategory",
+      data
+    );
+    setCategories(apiCategories.data.data);
+  };
+
+  useEffect(() => {
+    getImportProducts();
+    getCategories();
+  }, []);
+
+  useEffect(() => {
+    console.log("Selected Categories:", selectedCategories);
+  }, [selectedCategories]);
+
+  const handleCheckboxChange = (event) => {
+    const { checked, name } = event.target;
+    if (checked) {
+      setSelectedCategories([...selectedCategories, name]);
+    } else {
+      setSelectedCategories(selectedCategories.filter(category => category !== name));
+    }
+  };
+  
+  const filteredProducts = selectedCategories.length > 0 
+    ? importProducts.filter(product => selectedCategories.includes(product.category_name)) // I changed 'product.category' to 'product.category_name'
+    : importProducts;
+
+  const handleNavigateItem = (id) => {
+    navigate("/ImportItem/" + id);
+  };
 
   const handleView = async (id) => {
     try {
@@ -35,6 +71,7 @@ const Import = () => {
       console.error("Error viewing product:", error);
     }
   };
+
   const formatDate = (date) => {
     const now = moment();
     const created = moment(date);
@@ -49,44 +86,50 @@ const Import = () => {
       return `${Math.floor(diffInHours / (24 * 30))} months ago`;
     }
   };
-  
- 
 
   return (
     <div>
-      <NavBar />
-      <div className="d-flex justify-content-center  mt-4 text-primary">
+      <div className="d-flex justify-content-center mt-4 text-primary">
         <h1> Import List</h1>
+      </div>
+      <div className="d-flex justify-content-center mt-4">
+      {categories.map(category => (
+          <Form.Check 
+            type="checkbox" 
+            id={`category-${category.name}`} 
+            label={category.name} 
+            name={category.name} 
+            onChange={handleCheckboxChange} 
+            className="m-2"
+          />
+      ))}
       </div>
       <div>
         <div>
-          {importProducts.map((importProduct) => {
-            return (
-              <div
-                key={importProduct.id}
-                className="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto border m-3 p-4 border-dark  rounded"
-              >
-                <div>
-                  <p>country: {importProduct.country}</p>
-                  <p>price: {importProduct.price}</p>
-                  <p>name: {importProduct.name}</p>
-                  <p>description: {importProduct.description}</p>
-                  <p>created at: {formatDate(importProduct.created_at)}</p>
-                </div>
-
-                <div className="d-flex justify-content-center">
-                  <Button
-                    onClick={() => {
-                      handleNavigateItem(importProduct.id);
-                      handleView(importProduct.id);
-                    }}
-                  >
-                    View More
-                  </Button>
-                </div>
+          {filteredProducts.map((importProduct) => (
+            <div
+              key={importProduct.id}
+              className="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto border m-3 p-4 border-dark rounded">
+              <div>
+                <p>country: {importProduct.country}</p>
+                <p>price: {importProduct.price}</p>
+                <p>name: {importProduct.name}</p>
+                <p>description: {importProduct.description}</p>
+                <p>created at: {formatDate(importProduct.created_at)}</p>
               </div>
-            );
-          })}
+
+              <div className="d-flex justify-content-center">
+                <Button
+                  onClick={() => {
+                    handleNavigateItem(importProduct.id);
+                    handleView(importProduct.id);
+                  }}
+                >
+                  View More
+                </Button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
