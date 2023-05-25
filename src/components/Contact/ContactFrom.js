8b5e7ce6d9619c800efe87be6556379350e91f32
Bumirec <bumirec@gmail.com>
2023-05-25 13:16:25 +0200

    bug fix when data dosent show on form

--- src/components/Contact/ContactFrom.js
+++ src/components/Contact/ContactFrom.js
@@ -1,39 +1,39 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Form, InputGroup, Button } from "react-bootstrap";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 import "./ContactFrom.css";
 
 export const ContactFrom = () => {
   const { id } = useParams();
-
+  const [productData, setProductData] = useState(null);
   const [formValues, setFormValues] = useState({
     name: "",
     email: "",
     message: "",
-    Quantity: "",
   });
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:8000/api/form/${id}`)
-      .then(response => {
-        const { Product, email } = response.data;
-        setFormValues({
-          name: Product,
-          email: email,
-          message: `I am interested in your product: ${Product}`,
-          Quantity: formValues.Quantity
-        });
-        console.log(response.data);
-      })
-      .catch(error => console.error('Error fetching data: ', error));
-  }, [id]);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/api/form/2`);
+        setProductData(response.data[0]); // Set product data. Assume response is an array and we're interested in the first item
 
-  useEffect(() => {
-    console.log(formValues); // Log formValues whenever it changes
-  }, [formValues]);  // Add id as a dependency to useEffect
+        // Also update form values with product data
+        setFormValues({
+          name: response.data[0].Product,
+          email: response.data[0].email,
+          message: `I am interested in your product: ${response.data[0].Product}`,
+        });
+
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -41,85 +41,64 @@ export const ContactFrom = () => {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    axios
-      .get('http://localhost:8000/api/Notify/${Oid}/${Uid}/${Pid}', formValues)
-      .then(res => {
-        console.log('Data sent to database successfully.');
-        window.location.href = `mailto:${formValues.email}?subject=Interest in ${formValues.name}&body=${formValues.message}`;
-      })
-      .catch(err => {
-        console.log('Error in sending data to database.', err);
-      });
+    // e.preventDefault();
+    // axios
+    //   .get('http://localhost:8000/api/Notify/${Oid}/${Uid}/${Pid}', formValues)
+    //   .then(res => {
+    //     console.log('Data sent to database successfully.');
+    //     window.location.href = `mailto:${formValues.email}?subject=Interest in ${formValues.name}&body=${formValues.message}`;
+    //   })
+    //   .catch(err => {
+    //     console.log('Error in sending data to database.', err);
+    //   });
   };
 
+  if (!productData) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>
-      <div id="add-new-company-base">
-        <Form onSubmit={handleSubmit}>
-          <h1 className="text-center">Contact seller</h1>
-          <br></br>
-          <h5>Subject</h5>
-          <InputGroup className="mb-3">
-            <Form.Control
-              onChange={handleChange}
-              value={formValues.name}
-              name="name"
-              placeholder="Subject"
-              aria-label="Name"
-            />
-          </InputGroup>
-          <br></br>
-          <h5>Email</h5>
-          <InputGroup className="mb-3">
-            <Form.Control
-              readOnly
-              value={formValues.email}
-              name="email"
-              placeholder="Email"
-              aria-label="Email"
-              aria-describedby="basic-addon2"
-            />
-          </InputGroup>
-          <br></br>
-          <InputGroup className="mb-3">
-            <Form.Control
-              onChange={handleChange}
-              type="number"
-              min="0"
-              value={formValues.Quantity}
-              name="Quantity"
-              placeholder="Quantity"
-              aria-label="Quantity"
-              aria-describedby="basic-addon2"
-            />
-          </InputGroup>
-          <br></br>
-          <h5>Message</h5>
-          <InputGroup className="mb-3">
-            <Form.Control
-              as="textarea"
-              rows={3}
-              onChange={handleChange}
-              value={formValues.message}
-              name="message"
-              placeholder="Message"
-              aria-label="Message"
-              aria-describedby="basic-addon2"
-            />
-          </InputGroup>
-          <br></br>
-          <div className="d-flex justify-content-center">
-            <Button
-              variant="outline-secondary"
-              id="button-addon2"
-              type="submit"
-            >
-              Send
-            </Button>
-          </div>
-        </Form>
-      </div>
+    <div id="add-new-company-base">
+      <Form onSubmit={handleSubmit}>
+        <h1 className="text-center">Contact seller</h1>
+        <h5>Subject</h5>
+        <InputGroup className="mb-3">
+          <Form.Control
+            value={formValues.name}
+            name="name"
+            placeholder="Subject"
+            aria-label="Name"
+            onChange={handleChange}
+          />
+        </InputGroup>
+        <h5>Email</h5>
+        <InputGroup className="mb-3">
+          <Form.Control
+            readOnly
+            value={formValues.email}
+            name="email"
+            placeholder="Email"
+            aria-label="Email"
+          />
+        </InputGroup>
+        <h5>Message</h5>
+        <InputGroup className="mb-3">
+          <Form.Control
+            as="textarea"
+            rows={3}
+            value={formValues.message}
+            name="message"
+            placeholder="Message"
+            aria-label="Message"
+            onChange={handleChange}
+          />
+        </InputGroup>
+        <div className="d-flex justify-content-center">
+          <Button variant="outline-secondary" id="button-addon2" type="submit">
+            Send
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 };
