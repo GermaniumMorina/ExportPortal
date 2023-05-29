006c13f52459cce7ae83ce87b5ddae680ca64d77
Dafina Dellova <dafinadellova@gmail.com>
2023-05-29 15:57:25 +0200

    Language Change

--- src/index.js
+++ src/index.js
@@ -8,13 +8,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import NavBar from "./components/Navigation/NavBar";
 axios.defaults.withCredentials = true;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <NavBar />
       <App />
     </Router>
   </React.StrictMode>
