42cb53f8afa2da6812c5da06b6a6986fcd6740c5
rrezonk16 <rrezonkrasniqi32@gmail.com>
2023-05-29 11:12:17 +0200

    Fixed this>>>>>

--- src/components/Products/Import/ImportItem.js
+++ src/components/Products/Import/ImportItem.js
@@ -8,28 +8,43 @@ import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 import moment from "moment";
 import NavBar from "../../Navigation/NavBar";
 import { useTranslation } from "react-i18next";
+import LoadingBar from "../../LoadingScreens/LoadingBar.js";
+
+
+
+
 const ImportItem = () => {
   const navigate = useNavigate();
   const [importProduct, setImportProduct] = useState([]);
-  //calling the GET method to get the "import" data
-  const getImportProduct = async () => {
-    const data = {
-      headers: {
-        Accept: "application/json",
-      },
-    };
-
-    const response = await axios.get(
-      "http://127.0.0.1:8000/api/ilist/" + id,
-      data
-    );
-    const apiImportProducts = response.data[0][0];
-    setImportProduct(apiImportProducts);
-  };
+  const [isLoading, setIsLoading] = useState(true);
   let { id } = useParams();
+  const { t } = useTranslation();
+
+  const getImportProduct = async () => {
+    try {
+      const data = {
+        headers: {
+          Accept: "application/json",
+        },
+      };
+
+      const response = await axios.get(
+        `http://127.0.0.1:8000/api/ilist/${id}`,
+        data
+      );
+      const apiImportProducts = response.data[0][0];
+      setImportProduct(apiImportProducts);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error fetching import product:", error);
+    }
+  };
+
   useEffect(() => {
     getImportProduct(id);
-  });
+    //eslint-disable-next-line 
+      }, [id]);
+
   const handleBack = () => {
     navigate("/Import");
   };
@@ -48,7 +63,10 @@ const ImportItem = () => {
       return `${Math.floor(diffInHours / (24 * 30))} months ago`;
     }
   };
-  const { t } = useTranslation();
+
+  if (isLoading) {
+    return <LoadingBar/>
+  }
 
   return (
     <div>
@@ -58,53 +76,50 @@ const ImportItem = () => {
       </div>
       <div>
         <div className="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto   mb-4 p-5 border rounded  border-dark ">
-          {importProduct.map((importProduct) => {
-            return (
-              <div key={importProduct.id}>
-                <div>
-                  <p>
-                    {t("companies.Name")} {importProduct.name}
-                  </p>
-                  <p>
-                    {t("companies.Country")} {importProduct.country}
-                  </p>
-                  <p>
-                    {t("import.Price")}
-                    {importProduct.price}
-                  </p>
+          <div key={importProduct.id}>
+            <div>
+              <p>
+                {t("companies.Name")} {importProduct.name}
+              </p>
+              <p>
+                {t("companies.Country")} {importProduct.country}
+              </p>
+              <p>
+                {t("import.Price")}
+                {importProduct.price}
+              </p>
 
-                  <p>
-                    {t("import.Description")} {importProduct.description}
-                  </p>
-                  <p>
-                    {t("import.Created at")}
-                    {formatDate(importProduct.created_at)}
-                  </p>
-                  <p>
-                    {t("import.Views")} {importProduct.views}
-                  </p>
-                  <p>
-                    {t("company.Category")} {importProduct.category_name}
-                  </p>
-                  <a href="https://www.facebook.com/" className="m-2">
-                    <FaFacebook />
-                  </a>
-                  <a href="https://www.instagram.com/" className="m-2">
-                    <FaInstagram />
-                  </a>
-                  <a href="https://www.twitter.com/" className="m-2">
-                    <FaTwitter />
-                  </a>
-                </div>
-                <div className="d-flex justify-content-center btn-lg">
-                  <Button onClick={handleBack}>{t("import.Back")}</Button>
-                </div>
-              </div>
-            );
-          })}
+              <p>
+                {t("import.Description")} {importProduct.description}
+              </p>
+              <p>
+                {t("import.Created at")}
+                {formatDate(importProduct.created_at)}
+              </p>
+              <p>
+                {t("import.Views")} {importProduct.views}
+              </p>
+              <p>
+                {t("company.Category")} {importProduct.category_name}
+              </p>
+              <a href="https://www.facebook.com/" className="m-2">
+                <FaFacebook />
+              </a>
+              <a href="https://www.instagram.com/" className="m-2">
+                <FaInstagram />
+              </a>
+              <a href="https://www.twitter.com/" className="m-2">
+                <FaTwitter />
+              </a>
+            </div>
+            <div className="d-flex justify-content-center btn-lg">
+              <Button onClick={handleBack}>{t("import.Back")}</Button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 };
+
 export default ImportItem;
