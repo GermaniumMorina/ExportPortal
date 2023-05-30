db287dc1daa4799a26945c9b1bf1e8458c4b8773
Bumirec <bumirec@gmail.com>
2023-05-30 11:33:19 +0200

    bug fix on marketpalce

--- src/components/Company/Company.js
+++ src/components/Company/Company.js
@@ -11,23 +11,25 @@ const Company = () => {
   const [isLoading, setIsLoading] = useState(true);
   const { t } = useTranslation();
 
+  useEffect(() => {
+    getCompany();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const getCompany = async () => {
     try {
-      const response = await axios.get(`http://127.0.0.1:8000/api/company_details/${id}`);
-      setCompany(response.data.data);
+      const response = await axios.get(
+        `http://127.0.0.1:8000/api/company_details/${id}`
+      );
+      setCompany(response.data[0]);
       setIsLoading(false);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
 
-  useEffect(() => {
-    getCompany();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   if (isLoading) {
-    return <LoadingBar/>
+    return <LoadingBar />;
   }
 
   return (
@@ -36,41 +38,41 @@ const Company = () => {
       {company.length === 0 ? (
         <p>Loading...</p>
       ) : (
-        company.map((company) => (
-          <div key={company.id} className="d-flex justify-content-center  mt-4">
-            <div className="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto   mb-4 p-5 border rounded  border-dark ">
-              <h1>{company.name}</h1>
-
-              <img
-                src={company.profile_picture}
-                alt={company.name}
-                className="company-image"
-              />
-              <p>
-                {t("companies.Keywords")} {company.keywords}
-              </p>
-              <p>
-                {t("companies.Country")} {company.country}
-              </p>
-              <p>
-                {t("companies.Web Address")} {company.web_address || "N/A"}
-              </p>
-              <p>
-                {t("company.More Info")} {company.more_info}
-              </p>
-              <p>
-                {t("company.Category")} {company.category}
-              </p>
-              <p>
-                {t("company.Sub-Catgory")}
-                {company.subcategory}
-              </p>
-              <p>
-                {t("company.Membership")} {company.membership}
-              </p>
-            </div>
+        <div className="d-flex justify-content-center mt-4">
+          <div className="col-xl-5 col-lg-6 col-md-8 col-sm-10 mx-auto mb-4 p-5 border rounded border-dark">
+            {company.map((item) => (
+              <div key={item.id}>
+                <h1>{item.name}</h1>
+                <img
+                  src={item.profile_picture}
+                  alt={item.name}
+                  className="company-image"
+                />
+                <p>
+                  {t("companies.Keywords")} {item.keywords}
+                </p>
+                <p>
+                  {t("companies.Country")} {item.country}
+                </p>
+                <p>
+                  {t("companies.Web Address")} {item.web_address || "N/A"}
+                </p>
+                <p>
+                  {t("company.More Info")} {item.more_info}
+                </p>
+                <p>
+                  {t("company.Category")} {item.category}
+                </p>
+                <p>
+                  {t("company.Sub-Catgory")} {item.subcategory}
+                </p>
+                <p>
+                  {t("company.Membership")} {item.membership}
+                </p>
+              </div>
+            ))}
           </div>
-        ))
+        </div>
       )}
     </div>
   );
