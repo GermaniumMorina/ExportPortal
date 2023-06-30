9c80d0d288fe9582f991aeb49df1b6df7a608e7c
rrezonk16 <rrezonkrasniqi32@gmail.com>
2023-06-30 09:43:16 +0200

    Added the edit categories button

--- src/components/Admin/ManageCompanies.js
+++ src/components/Admin/ManageCompanies.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./ManageCompanies.css";
 import Button from "@mui/material/Button";
@@ -7,6 +7,15 @@ import { BsFillFileEarmarkBreakFill } from "react-icons/bs";
 import { MdOutlineDoneOutline } from "react-icons/md";
 import { ImNotification } from "react-icons/im";
 import { SiQuicklook } from "react-icons/si";
+import axios from "axios";
+import alertify from "alertifyjs";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import OutlinedInput from "@mui/material/OutlinedInput";
 
 const ManageCompanies = () => {
   const { id } = useParams();
@@ -19,11 +28,238 @@ const ManageCompanies = () => {
     setAnchorEl(null);
   };
 
+  const updateStatus = (value) => {
+    axios
+      .put(`http://127.0.0.1:8000/api/updateStatus/${id}`, { status_id: value })
+      .then((response) => {
+        console.log(response);
+        alertify.success(response.data);
+        handleClose();
+      })
+      .catch((error) => {
+        alertify.error("Status change failed");
+        console.error(error);
+      });
+  };
+
+  const handleCompanyDelete = () => {
+    alertify
+      .confirm()
+      .setting({
+        label: "Delete",
+        message: "Delete the company?",
+        onok: function () {
+          deleteCompany();
+        },
+        oncancel: function () {
+        },
+      })
+      .show();
+  };
+
+  const deleteCompany = () => {
+    const companyId = id; 
+    axios
+      .delete(`http://127.0.0.1:8000/api/deleteCompany/${companyId}`)
+      .then((response) => {
+        console.log(response);
+        alertify.success("Company deleted successfully.");
+      })
+      .catch((error) => {
+        alertify.error("Failed to delete company.");
+        console.error(error);
+      });
+  };
+
+  const [formData, setFormData] = useState({
+    name: "",
+    keywords: "",
+    country_id: "",
+    country:"",
+    web_address: "",
+    more_info: "",
+    membership: "",
+    category: "",
+    subcategory: ""
+  });
+  const [countryList, setCountryList] = useState([]);
+
+  const handleFormSubmit = () => {
+    axios
+      .put(`http://127.0.0.1:8000/api/updateCompany/${id}`, formData)
+      .then((response) => {
+        console.log(response);
+        alertify.success("Company updated successfully.");
+      })
+      .catch((error) => {
+        alertify.error("Failed to update company.");
+        console.error(error);
+      });
+  };
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value
+    }));
+  };
+
+  useEffect(() => {
+    // Fetch company details from the API
+    axios
+      .get(`http://127.0.0.1:8000/api/company_details/${id}`)
+      .then((response) => {
+        console.log(response);
+        const companyData = response.data[0];
+        setFormData({
+          name: companyData.name,
+          keywords: companyData.keywords,
+          country_id: companyData.country_id,
+          country: companyData.country,
+
+          web_address: companyData.web_address,
+          more_info: companyData.more_info,
+          membership: companyData.membership,
+          category: companyData.category,
+          subcategory: companyData.subcategory
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [id]);
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  const fetchCountries = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/api/country");
+      console.log(response)
+      setCountryList(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="mc-grid-container">
-      <div className="mc-left-column">ID: {id}</div>
+      <div className="mc-left-column">
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div className="mc-edit-form">
+            <TextField
+              required
+              id="filled-required"
+              label="Name"
+              name="name"
+              value={formData.name}
+              variant="filled"
+              onChange={handleInputChange}
+            />
+            <br/>
+            <TextField
+              required
+              id="filled-required"
+              label="Keywords"
+              name="keywords"
+              value={formData.keywords}
+              variant="filled"
+              onChange={handleInputChange}
+            />
+                        <br/>
+
+            <FormControl required sx={{ m: 1, width: "25ch" }}>
+              <InputLabel id="country-label">Country</InputLabel>
+              <Select
+                labelId="country-label"
+                id="country"
+                name="country"
+                value={formData.country}
+                onChange={handleInputChange}
+                input={<OutlinedInput label="Country" />}
+              >
+                {countryList.map((country) => (
+                  <MenuItem key={country.id} value={country.id} >
+                    {country.country}
+                  </MenuItem>
+                ))}
+              </Select>
+              <span>Country: {formData.country}</span>
+            </FormControl>
+                        <br/>
+
+            <TextField
+              required
+              id="filled-required"
+              label="Web Address"
+              name="web_address"
+              value={formData.web_address}
+              variant="filled"
+              onChange={handleInputChange}
+            />
+                        <br/>
+
+            <TextField
+              required
+              id="filled-required"
+              label="More Info"
+              name="more_info"
+              value={formData.more_info}
+              variant="filled"
+              onChange={handleInputChange}
+            />
+                        <br/>
+
+            <TextField
+              required
+              id="filled-required"
+              label="Membership"
+              name="membership"
+              value={formData.membership}
+              variant="filled"
+              onChange={handleInputChange}
+            />
+                        <br/>
+
+            <TextField
+              required
+              id="filled-required"
+              label="Category"
+              name="category"
+              value={formData.category}
+              variant="filled"
+              onChange={handleInputChange}
+            />
+                        <br/>
+
+            <TextField
+              required
+              id="filled-required"
+              label="Subcategory"
+              name="subcategory"
+              value={formData.subcategory}
+              variant="filled"
+              onChange={handleInputChange}
+            />
+  <br/>
+            <Button variant="contained" color="primary" onClick={handleFormSubmit}>
+              Update Company
+            </Button>
+          </div>
+        </Box>
+      </div>
       <div className="mc-right-column">
-        <div className="row">
+        <div className="mc-row">
+          <p className="mc-text">Manage the company application status</p>
           <Button
             id="basic-button"
             aria-controls={open ? "basic-menu" : undefined}
@@ -42,38 +278,44 @@ const ManageCompanies = () => {
               "aria-labelledby": "basic-button",
             }}
           >
-              <div className="main-announcments-div">
-              <div className="company-status is-preparing" onClick={handleClose}>
-              <SiQuicklook />
-              <span>Preparing </span>
-            </div>
-          
-            <div
-              className="company-status is-under"
-              onClick={handleClose}
-            >
-              <BsFillFileEarmarkBreakFill />
-              <span>Under Reviewal</span>
-            </div>
-            <div className="company-status is-approved" onClick={handleClose}>
-              <MdOutlineDoneOutline />
-              <span>Approved</span>
-            </div>
+            {/* ... */}
+            <div className="main-announcments-div">
+              <div
+                className="company-status is-preparing"
+                onClick={() => updateStatus(1)}
+              >
+                <SiQuicklook />
+                <span>Preparing </span>
+              </div>
 
-            <div
-              className="company-status is-disapproved"
-              onClick={handleClose}
-            >
-              <ImNotification />
-              <span>Disapproved</span>
-            </div>
+              <div
+                className="company-status is-under"
+                onClick={() => updateStatus(2)}
+              >
+                <BsFillFileEarmarkBreakFill />
+                <span>Under Reviewal</span>
+              </div>
+              <div
+                className="company-status is-approved"
+                onClick={() => updateStatus(3)}
+              >
+                <MdOutlineDoneOutline />
+                <span>Approved</span>
+              </div>
 
-         
+              <div
+                className="company-status is-disapproved"
+                onClick={() => updateStatus(4)}
+              >
+                <ImNotification />
+                <span>Disapproved</span>
+              </div>
             </div>
           </Menu>
-          
         </div>
-        <div className="mc-row">Row 2</div>
+        <div className="mc-row-2" onClick={handleCompanyDelete}>
+          <p className="mc-text">DELETE</p>
+        </div>
       </div>
     </div>
   );
--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -8,11 +8,11 @@ import { SiQuicklook } from "react-icons/si";
 import { ImNotification } from "react-icons/im";
 import { BsFillFileEarmarkBreakFill } from "react-icons/bs";
 import axios from "axios";
+import LoadingBar from "../LoadingScreens/LoadingBar";
 
 const Summary = () => {
   const UserName = localStorage.getItem("userName");
   const UserId = localStorage.getItem("userId");
-
   const UserEmail = localStorage.getItem("userEmail");
   const UserSurname = localStorage.getItem("userSurname");
   const UserPhone = localStorage.getItem("userPhone");
@@ -21,105 +21,202 @@ const Summary = () => {
 
   const [companies, setCompanies] = useState([]);
   const [notifications, setNotifications] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [editMode, setEditMode] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedSubcategory, setSelectedSubcategory] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [subcategories, setSubcategories] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [apiCompanies, apiNotifications] = await Promise.all([
-          axios.get(`http://127.0.0.1:8000/api/companyData/${UserId}`),
-          axios.get(`http://127.0.0.1:8000/api/showAllNotify/${UserId}/1`),
-        ]);
+        const [apiCompanies, apiNotifications, apiCategories, apiSubCategories] =
+          await Promise.all([
+            axios.get(`http://127.0.0.1:8000/api/companyData/${UserId}`),
+            axios.get(`http://127.0.0.1:8000/api/showAllNotify/${UserId}/1`),
+            axios.get("http://127.0.0.1:8000/api/category"),
+            axios.get("http://127.0.0.1:8000/api/subcategory"),
+
+          ]);
         setCompanies(apiCompanies.data);
         console.log(apiCompanies.data);
 
         setNotifications(apiNotifications.data.original);
         console.log(apiNotifications.data.original);
+
+        setCategories(apiCategories.data);
+        console.log(apiCategories.data);
+
+        setSubcategories(apiSubCategories.data)
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
+        setLoading(false);
       }
     };
 
     fetchData();
   }, [UserId]);
 
+  const handleEditClick = () => {
+    setEditMode(true);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      // Assuming you have the COMPANY_ID available
+      const COMPANY_ID =(companies[0].id) ; // Replace with the actual company ID
+      console.log(COMPANY_ID)
+      console.log({
+        category: selectedCategory,
+        subcategory: selectedSubcategory,
+      })
+      // Perform the PUT request to update the category and subcategory
+      await axios.put(`http://127.0.0.1:8000/api/updateCategory/${COMPANY_ID}`, {
+        category_id: selectedCategory,
+        subcategory_id: selectedSubcategory,
+      });
+
+      setEditMode(false);
+    } catch (error) {
+      console.error("Error updating category and subcategory:", error);
+    }
+  };
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+  const handleSubcategoryChange = (event) => {
+    setSelectedSubcategory(event.target.value);
+  };
+
   return (
     <div>
-      <div className="summary-main-div">
-        <div className="personal-info-left">
-          <div className="personal-information">
-            <img src={avatar} alt="avatar" className="user-avatar" />
-            <div className="user-details">
-              <h2>
-                {t("companies.Name")} {UserName} {UserSurname}
-              </h2>
-              <p>
-                {t("profileViewer.Email")} {UserEmail}
-              </p>
-              <p>
-                {t("profileViewer.Phone")} {UserPhone}
-              </p>
-              <p>
-                {t("profileViewer.Gender")} {UserGender}
-              </p>
+      {loading ? (
+        <div>
+          <LoadingBar />
+        </div>
+      ) : (
+        <div className="summary-main-div">
+          <div className="personal-info-left">
+            <div className="personal-information">
+              <img src={avatar} alt="avatar" className="user-avatar" />
+              <div className="user-details">
+                <h2>
+                  {t("companies.Name")} {UserName} {UserSurname}
+                </h2>
+                <p>
+                  {t("profileViewer.Email")} {UserEmail}
+                </p>
+                <p>
+                  {t("profileViewer.Phone")} {UserPhone}
+                </p>
+                <p>
+                  {t("profileViewer.Gender")} {UserGender}
+                </p>
+              </div>
+            </div>
+
+            <div className="personal-notifications">
+              <div className="notification-icon">
+                <NotificationsIcon />
+              </div>
+
+              {notifications.map((notification, index) => (
+                <div className="notification" key={index}>
+                  <p>{`${notification["Full Name"]} is interested in your product: ${notification["Product"]}`}</p>
+                </div>
+              ))}
             </div>
           </div>
+          <div className="company-info-right">
+            {companies.length > 0 && (
+              <>
+                <div className="company-information">
+                  {companies.map((company) => (
+                    <div key={company.id}>
+                      <div
+                        className={`company-status is-${company.status.toLowerCase()}`}
+                      >
+                        {company.status === "Approved" && (
+                          <MdOutlineDoneOutline />
+                        )}
+                        {company.status === "Under Reviewal" && (
+                          <BsFillFileEarmarkBreakFill />
+                        )}
+                        {company.status === "Disapproved" && (
+                          <ImNotification />
+                        )}
+                        {company.status === "Preparing" && <SiQuicklook />}
+                        <span>{company.status}</span>
+                      </div>
 
-          <div className="personal-notifications">
-            <div className="notification-icon">
-              <NotificationsIcon />
-            </div>
+                      <div className="company-details">
+                        {editMode ? (
+                          <>
+                            <label htmlFor="category">Category:</label>
+                            <select
+                              id="category"
+                              value={selectedCategory}
+                              onChange={handleCategoryChange}
+                            >
+                              <option value="">Select Category</option>
+                              {categories.map((category) => (
+                                <option key={category.id} value={category.id}>
+                                  {category.name}
+                                </option>
+                              ))}
+                            </select>
+                            <br />
+                            <br />
+                            <label htmlFor="subcategory">Subcategory:</label>
+                            <select
+                              id="subcategory"
+                              value={selectedSubcategory}
+                              onChange={handleSubcategoryChange}
+                            >
+                              <option value="">Select Subcategory</option>
+                              {subcategories.map((subcategory) => (
+                                <option
+                                  key={subcategory.id}
+                                  value={subcategory.id}
+                                >
+                                  {subcategory.name}
+                                </option>
+                              ))}
+                            </select>
+                            <br />
+                            <br />
+                            <button onClick={handleSubmit}>Submit</button>
+                          </>
+                        ) : (
+                          <>
+                            <h2>Name: {company.name}</h2>
+                            <br />
+                            <br />
+                            <br />
 
-            {notifications.map((notification, index) => (
-              <div className="notification" key={index}>
-                <p>{`${notification["Full Name"]} is interested in your product: ${notification["Product"]}`}</p>
-              </div>
-            ))}
-
+                            <p>Keywords: {company.keywords}</p>
+                            <p>Country: {company.country}</p>
+                            <p>Web Address: {company.web_address}</p>
+                            <p>More Info: {company.more_info}</p>
+                            <p>Type: {company.type}</p>
+                            <p>Category ID: {company.category_id}</p>
+                            <p>Subcategory ID: {company.subcategory_id}</p>
+                            <button onClick={handleEditClick}>Edit</button>
+                          </>
+                        )}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </>
+            )}
           </div>
         </div>
-        <div className="company-info-right">
-  {companies.length > 0 && (
-    <>
-      <div className="company-information">
-        {companies.map((company) => (
-          <div key={company.id}>
-            <div
-              className={`company-status is-${company.status.toLowerCase()}`}
-            >
-              {company.status === "Approved" && <MdOutlineDoneOutline />}
-              {company.status === "Under Reviewal" && (
-                <BsFillFileEarmarkBreakFill />
-              )}
-              {company.status === "Disapproved" && <ImNotification />}
-              {company.status === "Preparing" && <SiQuicklook />}
-              <span>{company.status}</span>
-            </div>
-
-            <div className="company-details">
-              <h2>Name: {company.name}</h2>
-              <br/>
-              <br/>
-              <br/>
-
-
-              <p>Keywords: {company.keywords}</p>
-              <p>Country: {company.country}</p>
-              <p>Web Address: {company.web_address}</p>
-              <p>More Info: {company.more_info}</p>
-              <p>Budget: {company.budget}</p>
-              <p>Type: {company.type}</p>
-              <p>Category ID: {company.category_id}</p>
-              <p>Subcategory ID: {company.subcategory_id}</p>
-              <p>Membership: {company.membership}</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </>
-  )}
-</div>
-
-      </div>
+      )}
     </div>
   );
 };
